82a543851ef83c34ecba7b6c5a519b5d99306f3f
Lukas Jurk <lukas.jurk@tu-dresden.de>
2021-05-19 12:17:47 +0200

    Bump version: 0.0.1 → 0.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='ttrac',
-    version='0.0.1',
+    version='0.0.2',
     packages=['ttrac'],
     author="Lukas Jurk",
     author_email="ljurk@pm.me",
