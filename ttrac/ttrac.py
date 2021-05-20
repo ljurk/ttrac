f48db435f0355e666452985b1304bb01f8405aa9
Lukas Jurk <lukas.jurk@tu-dresden.de>
2021-05-20 13:05:10 +0200

    move data file to .config; add help text; cleanup code

--- ttrac/ttrac.py
+++ ttrac/ttrac.py
@@ -1,3 +1,4 @@
+import os.path
 from datetime import datetime
 import json
 import click
@@ -6,11 +7,17 @@ from terminaltables import AsciiTable
 timeformat = "%H:%M:%S"
 strptimeformat = "%d-%m-%Y %H:%M:%S"
 dayformat = "%d-%m-%Y"
+defaultfile=os.path.expanduser('~/.config/ttrac/data.json')
+
 def now():
     return str(datetime.now().strftime(timeformat))
 
 def today():
     return str(datetime.now().strftime(dayformat))
+
+def clear(filehandle):
+    filehandle.seek(0)
+    filehandle.truncate()
 
 @click.group()
 def cli():
@@ -18,49 +25,59 @@ def cli():
 
 @cli.group(name='break')
 def _break():
+    """combines subcommand that allows you to take a break"""
     pass
 
 @_break.command()
-def start(day=today()):
-    with open('data.json') as fh:
-        data = json.load(fh)
+@click.argument('--file', type=click.File('r+'), default=defaultfile)
+def start(__file, day=today()):
+    """start a break"""
+    try:
+        data = json.load(__file)
+    except json.decoder.JSONDecodeError:
+        exit(f'cannot parse json from {defaultfile}')
     if 'breaks' not in data[day].keys():
         data[day]['breaks'] = [{'start': now()}]
     else:
         if 'stop' not in data[day]['breaks'][-1].keys():
-            print("no break started yet")
-            return
+            exit("previous break hasnt stopped yet")
         data[day]['breaks'].append({'start': now()})
 
-    with open('data.json', 'w') as fh:
-        fh.write(json.dumps(data, indent=4))
-    print("ok")
+    clear(__file)
+    __file.write(json.dumps(data, indent=4))
+    click.echo("OK")
 
 @_break.command()
-def stop(day=today()):
-    with open('data.json') as fh:
-        data = json.load(fh)
+@click.argument('--file', type=click.File('r+'), default=defaultfile)
+def stop(__file, day=today()):
+    """stop a break"""
+    try:
+        data = json.load(__file)
+    except json.decoder.JSONDecodeError:
+        exit(f'cannot parse json from {defaultfile}')
     if 'breaks' not in data[day].keys() or 'stop' in data[day]['breaks'][-1].keys():
-        print("no break started yet")
-        return
+        exit("no break started yet")
     else:
         data[day]['breaks'][-1]['stop'] = now()
 
-    with open('data.json', 'w') as fh:
-        fh.write(json.dumps(data, indent=4))
-    print("ok")
+    clear(__file)
+    __file.write(json.dumps(data, indent=4))
+    click.echo("OK")
 
 
 def printTable(table_data):
     table = AsciiTable(table_data)
-    #table.inner_heading_row_border = False
     print(table.table)
 
 @cli.command()
 @click.option('-t', '--total', is_flag=True, default=False)
-def status(total, day=today()):
-    with open('data.json') as fh:
-        data = json.load(fh)
+@click.argument('--file', type=click.File('r'), default=defaultfile)
+def status(total, __file, day=today()):
+    """show all tracked times of the given day"""
+    try:
+        data = json.load(__file)
+    except json.decoder.JSONDecodeError:
+        exit(f'cannot parse json from {defaultfile}')
     for i in data:
         if i != day and not total:
             continue
@@ -73,12 +90,9 @@ def status(total, day=today()):
         ]
 
         if 'breaks' in data[i].keys():
-            #breaks = [["Start", "Stop", "Duration"]]
             table_data.append(["Breaks", ""])
             for num, y in enumerate(data[i]['breaks']):
                 if 'stop' in y:
-                    print(f"{i} {y['start']}")
-                    print("{i} {y['stop']}")
                     delta = abs(datetime.strptime(f"{i} {y['start']}", strptimeformat) - datetime.strptime(f"{i} {y['stop']}", strptimeformat))
                 else:
                     delta = abs(datetime.strptime(f"{i} {y['start']}", strptimeformat) - datetime.now())
@@ -86,34 +100,56 @@ def status(total, day=today()):
                 table_data = table_data + [[f'{delim} start', y['start']],
                                [f'{delim} stop', y['stop'] if 'stop' in y else '-'],
                                [f'{delim} duration', delta]]
-            #printTable(breaks)
         printTable(table_data)
 
 
 @cli.command()
-def start(day=today()):
-    with open('data.json') as fh:
-        data = json.load(fh)
+@click.argument('--file', type=click.File('r+'), default=defaultfile)
+def start(__file, day=today()):
+    """start timetracking"""
+    try:
+        data = json.load(__file)
+    except json.decoder.JSONDecodeError:
+        data = {day: {}}
     if day not in data:
         data[day] = {}
     data[day]['start'] = now()
-    with open('data.json', 'w') as fh:
-        fh.write(json.dumps(data, indent=4))
-    print(data)
+    clear(__file)
+    __file.write(json.dumps(data, indent=4))
     click.echo("OK")
 
 
 @cli.command()
-def stop(day=today()):
-    with open('data.json') as fh:
-        data = json.load(fh)
+def file():
+    """prints path to the data file """
+    print(defaultfile)
+
+@cli.command()
+@click.argument('--file', type=click.File('r'), default=defaultfile)
+def cat(__file):
+    """prints content of the data file """
+    print(__file.read())
+
+@cli.command()
+@click.argument('--file', type=click.File('r+'), default=defaultfile)
+def stop(__file, day=today()):
+    """stop timetracking"""
+    try:
+        data = json.load(__file)
+    except json.decoder.JSONDecodeError:
+        print(f'cannot parse json from {defaultfile}')
+        return
     if day not in data and 'start' not in data[day]:
         print("not started yet")
         return
     data[day]['stop'] = now()
-    with open('data.json', 'w') as fh:
-        fh.write(json.dumps(data, indent=4))
+    clear(__file)
+    __file.write(json.dumps(data, indent=4))
     click.echo("OK")
 
 if __name__ == '__main__':
+    if not os.path.isfile(defaultfile):
+        os.makedirs(os.path.dirname(defaultfile), exist_ok=True)
+        open(defaultfile, 'a').close()
+
     cli()
